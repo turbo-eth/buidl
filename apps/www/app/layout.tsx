5cd8badd465b9307c6cb73c0fefd3c7669fea1cb
Kames <sudo@kames.me>
2023-11-27 06:46:11 -0500

    fix: og

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -29,6 +29,9 @@ export const metadata: Metadata = {
     "Tailwind CSS",
     "Server Components",
     "Radix UI",
+    "WAGMI",
+    "Web3",
+    "Ethereum"
   ],
   authors: [
     {
@@ -48,20 +51,11 @@ export const metadata: Metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [siteConfig.ogImage],
     creator: "@districtLabs",
   },
   icons: {
