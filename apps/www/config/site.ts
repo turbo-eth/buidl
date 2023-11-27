88200533283c4098064243fb27668274b52ca850
Kames <sudo@kames.me>
2023-11-27 06:49:21 -0500

    fix: og

--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "turboeth/buidl",
-  url: "https://buidl.turboeth.com",
-  ogImage: "https://buidl.turboeth.com/og.jpg",
+  url: "https://buidl.turboeth.xyz",
+  ogImage: "https://buidl.turboeth.xyz/og.jpg",
   description:
     "Collection of essential components for modern Web3 applications ⚡️ copy and paste in minutes.",
   links: {
