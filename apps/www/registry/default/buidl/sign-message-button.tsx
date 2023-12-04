2ad00d9a5792e9cd6471239e6ea53a397d973175
Vitor Marthendal Nunes <marthendalnunes@gmail.com>
2023-12-04 13:53:48 -0300

    Feat: add cli (#4)
    * feat: add cli
    
    * fix: formatting

--- apps/www/registry/default/buidl/sign-message-button.tsx
+++ apps/www/registry/default/buidl/sign-message-button.tsx
@@ -8,38 +8,52 @@ import { Button, ButtonProps } from "@/registry/default/ui/button"
 
 interface SignMessageButtonProps extends Omit<ButtonProps, "onCick"> {
   message: string
+  displayError?: boolean
   onSuccess?: (signature: string) => void
 }
 
 const SignMessageButton = React.forwardRef<
   HTMLButtonElement,
   SignMessageButtonProps
->(({ message, onSuccess, className, children, disabled, ...props }, ref) => {
-  const { isLoading, isError, error, signMessage } = useSignMessage({
-    message,
-    onSuccess,
-  })
+>(
+  (
+    {
+      message,
+      onSuccess,
+      className,
+      children,
+      disabled,
+      displayError = true,
+      ...props
+    },
+    ref
+  ) => {
+    const { isLoading, isError, error, signMessage } = useSignMessage({
+      message,
+      onSuccess,
+    })
 
-  return (
-    <>
-      <Button
-        ref={ref}
-        disabled={disabled || isLoading || message.length === 0}
-        onClick={() => signMessage()}
-        className={className}
-        {...props}
-      >
-        {isLoading ? "Check Wallet" : children ?? "Sign Message"}
-      </Button>
-      {isError && (
-        <ErrorMessage
-          error={error}
-          defaultErrorMessage="Error while signing message"
-        />
-      )}
-    </>
-  )
-})
+    return (
+      <>
+        <Button
+          ref={ref}
+          disabled={disabled || isLoading || message.length === 0}
+          onClick={() => signMessage()}
+          className={className}
+          {...props}
+        >
+          {isLoading ? "Check Wallet" : children ?? "Sign Message"}
+        </Button>
+        {isError && displayError && (
+          <ErrorMessage
+            error={error}
+            defaultErrorMessage="Error while signing message"
+          />
+        )}
+      </>
+    )
+  }
+)
 
 SignMessageButton.displayName = "SignMessageButton"
 
--- apps/www/scripts/build-registry.ts
+++ apps/www/scripts/build-registry.ts
@@ -80,7 +80,7 @@ for (const style of styles) {
   }
 
   for (const item of result.data) {
-    if (item.type !== "components:buidl") {
+    if (item.type !== "components:buidl" && item.type !== "components:ui") {
       continue
     }
 
@@ -122,7 +122,9 @@ fs.writeFileSync(
 // ----------------------------------------------------------------------------
 // Build registry/index.json.
 // ----------------------------------------------------------------------------
-const names = result.data.filter((item) => item.type === "components:buidl")
+const names = result.data.filter(
+  (item) => item.type === "components:buidl" || item.type === "components:ui"
+)
 const registryJson = JSON.stringify(names, null, 2)
 rimraf.sync(path.join(REGISTRY_PATH, "index.json"))
 fs.writeFileSync(path.join(REGISTRY_PATH, "index.json"), registryJson, "utf8")
